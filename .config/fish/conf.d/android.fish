20d46f54798e0f8fdf97c90b20f181676c044c13
Mark Darren <152751762+BootlegYouki@users.noreply.github.com>
2026-09-14 07:53:27 +0800

    auto-sync: 2026-09-14 07:53

--- .config/fish/conf.d/android.fish
+++ .config/fish/conf.d/android.fish
@@ -3,7 +3,7 @@ set -gx ANDROID_SDK_ROOT "$HOME/Android/Sdk"
 set -gx ANDROID_AVD_HOME "$HOME/.android/avd"
 set -gx JAVA_HOME "/usr/lib/jvm/java-17-openjdk"
 
-fish_add_path -g "$ANDROID_HOME/emulator"
-fish_add_path -g "$ANDROID_HOME/platform-tools"
-fish_add_path -g "$ANDROID_HOME/cmdline-tools/latest/bin"
-fish_add_path -g "$JAVA_HOME/bin"
+# fish_add_path "$ANDROID_HOME/emulator" # excluded to avoid shadowing /usr/bin/qemu-img with ancient Android 2.12 qemu-img; emulator is symlinked in ~/.local/bin/
+fish_add_path -a -g "$ANDROID_HOME/platform-tools"
+fish_add_path -a -g "$ANDROID_HOME/cmdline-tools/latest/bin"
+fish_add_path -a -g "$JAVA_HOME/bin"
